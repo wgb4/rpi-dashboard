04758385611e3e15f952422f6fb6065b4a741ef2
wgb4 <wgb4@kent.ac.uk>
2024-12-13 20:01:41 +0000

    remove option for command

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ import subprocess
 def main():
     # Start cec-client in background
     cec_process = subprocess.Popen(
-        ["cec-client", "-s", "-d", "1"],
+        ["cec-client", "-d", "1"],
         stdin = subprocess.PIPE,
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE,
