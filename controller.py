b280b654331e1dee57dc0f0cf8baedab1f781e47
wgb4 <wgb4@kent.ac.uk>
2024-12-13 18:26:08 +0000

    added wait for ready

--- controller.py
+++ controller.py
@@ -10,6 +10,17 @@ def main():
         text = True
     )
 
-    print(cec_process.stdout.readline)
+    # Wait for cec to be ready for input
+    wait_for_ready(cec_process)
+    
+def wait_for_ready(process):
+    while True:
+        output = process.stdout.readline()
+        if output:
+            print(output.strip())
+            if "waiting for input" in output:
+                print("Readty for input...")
+                break
+
 
 main()
