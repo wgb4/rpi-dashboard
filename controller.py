b6a0c2bf1196df93163d6301129df66336f4363c
wgb4 <wgb4@kent.ac.uk>
2024-12-13 21:58:36 +0000

    added moonlight

--- controller.py
+++ controller.py
@@ -3,12 +3,13 @@ import subprocess
 def main():
     # Start cec-client in background
     cec_process = connect_cec()
+    moonlight_process = start_moonlight()
 
     # Wait for cec to be ready for input
     wait_for_ready(cec_process)
 
-    # turn_on_tv(cec_process)
-    turn_off_tv(cec_process)
+    turn_on_tv(cec_process)
+    #turn_off_tv(cec_process)
 
     print("Exiting")
     disconnect_cec(cec_process)
@@ -16,6 +17,16 @@ def main():
 def connect_cec():
     return subprocess.Popen(
         ["cec-client"],
+        stdin = subprocess.PIPE,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        bufsize = 1
+    )
+
+def start_moonlight():
+    return subprocess.Popen(
+        ["moonlight-qt", "stream", "192.168.68.11", "Desktop"],
         stdin = subprocess.PIPE,
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE,
@@ -35,6 +46,11 @@ def turn_on_tv(process):
     print("Turning on TV")
     process.stdin.write("on 0\n")
     process.stdin.flush()
+    for line in iter(process.stdout.readline, ''):
+        print(line.strip())
+        if "power status changed" in line:
+            print("Ready")
+            break
 
 def turn_off_tv(process):
     print("Turning off TV")
