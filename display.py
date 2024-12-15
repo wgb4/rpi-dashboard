73536ae130008a732e68ad188163e37322895914
wgb4 <wgb4@kent.ac.uk>
2024-12-15 16:00:06 +0000

    better start

--- display.py
+++ display.py
@@ -1,12 +1,23 @@
+import sys
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
 
-def on_activate(app):
-    win = Gtk.ApplicationWindow(application=app)
-    win.present()
 
-app = Gtk.Application()
-app.connect('activate', on_activate)
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Things will go here
 
-app.run(None)
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="com.example.GtkApplication")
+app.run(sys.argv)
