85ff3635ff97f810967f3da66965dd34e110b9ab
wgb4 <wgb4@kent.ac.uk>
2024-12-15 15:45:51 +0000

    bsaic

--- display.py
+++ display.py
@@ -1,21 +1,12 @@
-import sys
-
 import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
 
-gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    win.present()
 
+app = Gtk.Application()
+app.connect('activate', on_activate)
 
-class MyApplication(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
-
-    def do_activate(self):
-        window = Gtk.ApplicationWindow(application=self, title="Hello World")
-        window.present()
-
-
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+app.run(None)
